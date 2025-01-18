ba9615c262dd92dfbb88accde9f28297d5bc4992
daitomiun <mateo.rodriguez.fierro@gmail.com>
2025-01-17 19:13:49 -0500

    Add deletion functionality

--- cmdline.py
+++ cmdline.py
@@ -1,7 +1,7 @@
 import argparse
 from pathlib import Path
 from PyPDF2 import PdfReader, PdfWriter
-
+import itertools
 
 class Cmdline():
     def __init__(self) -> None:
@@ -25,7 +25,7 @@ class Cmdline():
             type=self.parse_split
         )
         self.parser.add_argument(
-            "-d", "--del",
+            "-d", "--delete",
             help="It deletes a range of pages '--del 1-5' or a single page '--del 1 3 5' from a PDF and makes a copy with the deleted pages",
             nargs="+",
             type=self.parse_split
@@ -34,9 +34,7 @@ class Cmdline():
         self.input_pdf = PdfReader(self.args.file_path)
 
     def check_valid_range(self, page):
-        print(f"{page}")
         num_pages = len(self.input_pdf.pages)
- 
         if page > num_pages:
             self.parser.error(f"ERR: page {page} range is higher than num of pages {num_pages}")
 
@@ -54,25 +52,42 @@ class Cmdline():
            self.parser.error(f"ERR: Page {single_page} has to be positive")
        return [int(value) - 1]
 
-    def getNumPages(self):
+    def get_num_pages(self):
         print(f"There are {len(self.input_pdf.pages)} pages at document {self.args.file_path}")
 
     def split_pdf(self):
-
+        print("--- split action ---")
         for i, page_range in enumerate(self.args.split, 1):
-                    writer = PdfWriter()
-                    for page in page_range:
-                        self.check_valid_range(page)
-                        writer.add_page(self.input_pdf.pages[page])
+            writer = PdfWriter()
+            for page in page_range:
+                self.check_valid_range(page)
+                writer.add_page(self.input_pdf.pages[page])
 
-                    split = self.args.split[i - 1]
-                    file_name = Path(self.args.file_path).stem
-                    start_split = split[0]
-                    end_split = split[len(split) - 1]
-                    pdf_path = f"./output/{file_name}_from_{start_split}_to_{end_split}.pdf"
-                    with open(pdf_path, "wb") as fp:
-                        writer.write(fp)
-                    print(f"--- PDF split number {i} ---")
+                split = self.args.split[i - 1]
+                file_name = Path(self.args.file_path).stem
+                start_split = split[0]
+                end_split = split[len(split) - 1]
+                pdf_path = f"./output/{file_name}_from_{start_split + 1}_to_{end_split + 1}.pdf"
+                with open(pdf_path, "wb") as fp:
+                    writer.write(fp)
+            print(f"--- DONE: PDF split on File {i} ---")
+
+    def del_range_pdf(self):
+        print("--- delete action ---")
+        writer = PdfWriter()
+        flattened_omit_list = [item for sublist in self.args.delete for item in sublist]
+        total_pages = list(range(0, len(self.input_pdf.pages)))
+        pages_to_keep = [i for i in total_pages if i not in flattened_omit_list]
+
+        for page in pages_to_keep:
+           writer.add_page(self.input_pdf.pages[page])
+
+        file_name = Path(self.args.file_path).stem
+        pdf_path = f"./output/{file_name}_del_selected_pages.pdf"
+        with open(pdf_path, "wb") as fp:
+            writer.write(fp)
+        print(f"--- PDF deletion at pages {flattened_omit_list}  ---")
+        print(f"--- DONE: PDF deletion done at file  {pdf_path} ---")
 
 
 
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from cmdline import Cmdline
 
 def main():
-    print("----- PDF SPLITS CLI TOOL -----")
+    print("----------------- SPLITS: PDF CLI editor -----------------")
     app = Cmdline()
-
     if app.args.total_pages:
-        app.getNumPages()
+        app.get_num_pages()
     if app.args.split:
         app.split_pdf()
+    if app.args.delete:
+        app.del_range_pdf()
+
+
 
 if __name__ == "__main__":
     main()
